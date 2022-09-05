50f40ff040305b43a4c20f32673e541712806f35
aman <amansinghtomar150@gmail.com>
2022-09-05 22:11:06 +0530

    SettingPage

--- src/Routing/Routing.js
+++ src/Routing/Routing.js
@@ -5,6 +5,7 @@ import Dashboard from "../pages/Dashboard/Dashboard";
 import Login from "../pages/Login/Login";
 import ProductDetails from "../pages/ProductDetails/ProductDetails";
 import Profile from "../pages/Profile/Profile";
+import Setting from "../pages/Setting/Setting";
 import Signup from "../pages/Signup/Signup";
 import PrivateRoutes from "./PrivateRoutes";
 
@@ -15,7 +16,8 @@ function Routing() {
       <Route path="login" element={<Login />} />
       <Route path="signup" element={<Signup />} />
       <Route path="profile" element={<Profile />} />
-      <Route path="product/:id" element={<ProductDetails />}/>
+      <Route path="product/:id" element={<ProductDetails />} />
+      <Route path="setting" element={<Setting/>}/>
       <Route
         path="cart"
         element={
